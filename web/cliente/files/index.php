8b0c6db22ff2399ce8ea8f1c01d00f7a6606a916
Roberto Paes <robertocpaes@gmail.com>
2021-12-25 20:08:06 -0300

    correcao index.php

--- web/cliente/files/index.php
+++ web/cliente/files/index.php
@@ -1,15 +1,17 @@
 <?php
-function dirToArray($dir) {
-   $nomemodpack = str_replace("files/", "", $dir);
+function dirToArray($dir,$modpack) {
    $cdir = scandir($dir);
    foreach ($cdir as $key => $value){
+
       if (!in_array($value,array(".",".."))){
          if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
-            dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+            dirToArray($dir . DIRECTORY_SEPARATOR . $value, $modpack);
          } else {
+
             $hash = hash_file('sha1', $dir . "/" . $value);
             $size = filesize($dir . "/" . $value);
-            $path = str_replace($dir."/", "", $nomemodpack . "/" . $value);
+            $path = str_replace("files/".$modpack."/", "", $dir . "/" . $value);
+
             $url = "http://" . $_SERVER['HTTP_HOST'] ."/cliente/files/". $dir . "/" . $value;
             if (strpos($path, "libraries") !== false) {
                $type = "LIBRARY";
@@ -32,13 +34,16 @@ $json = json_decode($json_file); // decode the JSON into an associative array
 foreach ( $json as $e )
     {
        if($e->id == $identificador){
-          return "files/".$e->directory;
+          return $e->directory;
        }
     }
 }
 
 header("Content-Type: application/json; charset=UTF-8");
 if (isset($_GET['modpack'])) {
-echo "[", dirToArray(getDirectory($_GET['modpack'])), "\"\"]";
+$modpack = getDirectory($_GET['modpack']);
+$dir = "files/".$modpack;
+echo "[", dirToArray($dir,$modpack), "\"\"]";
+//echo dirToArray(getDirectory($_GET['modpack']));
 }
 ?>
