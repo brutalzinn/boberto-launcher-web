760d1c929510fbee0b8cc5ebf32eab9ad690f478
Roberto Paes <robertocpaes@gmail.com>
2022-01-17 09:07:30 -0300

    add refactor

--- web/index.php
+++ web/index.php
@@ -2,9 +2,10 @@
     header('Content-Type: application/json');
     require_once 'vendor/autoload.php';
     use App\Services\RedisService;
+    use App\Controllers;
+use App\Controllers\ApiController;
 
-
-    include 'route.php';
+include 'route.php';
     Predis\Autoloader::register();
     $client = new Predis\Client([
         'scheme' => 'tcp',
@@ -15,37 +16,60 @@
     RedisService::Init($client);
     $api_key = getenv('API_HEADER');
 
+    define('BASEPATH','/');
 
 
     $route = new Route();
     //url, controller, method of controller, accept url params
-    $route->add('/','Api','index');
-    $route->add('/teste','Api','teste', true);
-    $route->add('/launcher/modpacks/list','Launcher','list_modpacks');
-    $route->add('/launcher/config','Launcher','launcher_Config');
-    $route->add('/launcher/modpacks/upload','Launcher','uploadFile');
-    $route->add('/launcher/version/upload','Launcher','uploadLauncherZips');
-    $route->add('/launcher/version','Launcher','updateLauncherVersion');
-    $route->add('/modpackcreator/modpacks/sync','ModPackManager','syncModPack');
-    $route->add('/modpackcreator/modpacks/append','ModPackManager','appendModPack');
-    $route->add('/redis/del','Redis','delRedis');
-    $route->add('/redis/clear','Redis','clearRedis');
+    Route::add('/',function() {
+        $controller = new ApiController();
+        return $controller->index();
+      },'get');
+    Route::add('/teste','Api','teste', true);
+    Route::add('/launcher/modpacks/list','Launcher','list_modpacks');
+    Route::add('/launcher/config','Launcher','launcher_Config');
+    Route::add('/launcher/modpacks/upload','Launcher','uploadFile');
+    Route::add('/launcher/version/upload','Launcher','uploadLauncherZips');
+    Route::add('/launcher/version','Launcher','updateLauncherVersion');
+    Route::add('/modpackcreator/modpacks/sync','ModPackManager','syncModPack');
+    Route::add('/modpackcreator/modpacks/append','ModPackManager','appendModPack');
+    Route::add('/redis/del','Redis','delRedis');
+    Route::add('/redis/clear','Redis','clearRedis');
     #if the content has id, the news will be updated
-    $route->add('/launcher/news/update','News','updateNews');
-    $route->add('/launcher/news','News','readNews', true);
-    $route->add('/launcher/news/del','News','deleteNews');
+    Route::add('/launcher/news/update','News','updateNews');
+    Route::add('/launcher/news','News','readNews', true);
+    Route::add('/launcher/news/del','News','deleteNews');
 
+
+    Route::pathNotFound(function($path) {
+        // Do not forget to send a status header back to the client
+        // The router will not send any headers by default
+        // So you will have the full flexibility to handle this case
+        header('HTTP/1.0 404 Not Found');
+       
+        echo 'Error 404 :-(<br>';
+        echo 'The requested path "'.$path.'" was not found!';
+      });
+      
+      // Add a 405 method not allowed route
+      Route::methodNotAllowed(function($path, $method) {
+        // Do not forget to send a status header back to the client
+        // The router will not send any headers by default
+        // So you will have the full flexibility to handle this case
+        header('HTTP/1.0 405 Method Not Allowed');
+        echo 'Error 405 :-(<br>';
+        echo 'The requested path "'.$path.'" exists. But the request method "'.$method.'" is not allowed on this path!';
+      });
 
     try
     {
-        $request_headers = getallheaders();
-        if(!isset($request_headers[$api_key]) || isset($request_headers[$api_key]) && $request_headers[$api_key] != getenv('API_TOKEN')) {
-            http_response_code(401);
-            echo json_encode(array('status' => false, 'data' => 'API-KEY DONT PROVIDED OR API-KEY IS WRONG.'), JSON_UNESCAPED_UNICODE);
-            exit;
-        }
-        http_response_code(200);
-        echo json_encode(array('status' => true, 'data' => $route->submit()), JSON_UNESCAPED_UNICODE);
+        // $request_headers = getallheaders();
+        // if(!isset($request_headers[$api_key]) || isset($request_headers[$api_key]) && $request_headers[$api_key] != getenv('API_TOKEN')) {
+        //     http_response_code(401);
+        //     echo json_encode(array('status' => false, 'data' => 'API-KEY DONT PROVIDED OR API-KEY IS WRONG.'), JSON_UNESCAPED_UNICODE);
+        //     exit;
+        // }
+        Route::run(BASEPATH);
         exit;
     } catch (Exception $e) {
         http_response_code(404);
