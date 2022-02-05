bac6fa945dc0ca2fa87756f14056813f1c6e4e83
Roberto Paes <robertocpaes@gmail.com>
2022-02-05 15:44:45 -0300

    hotfix

--- web/App/Controllers/BaseController.php
+++ web/App/Controllers/BaseController.php
@@ -7,7 +7,7 @@
         public static  $_news_file = "cliente/launcher/news-launcher/news-launcher.json";
         public static  $_launcher_package_file = "cliente/launcher/package.json";
         public static  $_launcher_config_file = "cliente/launcher/config-launcher/config.json";
-        public static  $_modpacks_dir = "cliente/files/files/";
+        public static  $_modpacks_dir = "cliente/files/";
         public static  $_launcher_update_dir = "cliente/launcher/update-launcher/";
 
 
